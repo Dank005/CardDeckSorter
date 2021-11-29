6abdf8e9a390b825b01695687f44eba4b19142e6
Dank005 <daniil.yushkov2000@mail.ru>
2021-11-29 20:35:40 +0500

    i hope it works

---
+++ WebAPIapplication/Controllers/ColodsController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPIapplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ColodsController : ControllerBase
+    {
+        Database packs;
+
+        public ColodsController(Database colods)
+        {
+            this.packs = colods;
+        }
+
+        //GET api/colods (get list of pack's name)
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {                   
+            return from pack in packs
+                   select pack.name;
+        }
+
+        //GET api/colods/name (get pack by name)
+        [HttpGet("{name}")]
+        public IEnumerable<List<string>> Get(string name)
+        {
+            return from pack in packs
+                   where pack.name == name
+                   select pack.cards;
+        }
+
+        //POST api/colods (create pack)
+        //in Body (example) { "name": "colodName", "countCards": 52 }
+        [HttpPost]
+        public void Post([FromBody] Pack value)
+        {
+            packs.Add(new Pack(value.name, value.countCards));
+        }
+
+        //DELETE api/colods/name (delete pack)
+        [HttpDelete("{name}")]
+        public void Delete(string name)
+        {
+            packs.RemoveAll(colod => colod.name==name);
+        }
+
+        //PUT api/colods/name/mixer (mix pack)
+        //mixer - "simple"
+        [HttpPut("{name}/{mixer}")] 
+        public void Mix(string name, string mixer)
+        {
+            if (mixer.Equals("simple"))
+                foreach (var col in packs)
+                {
+                    if (col.name == name)
+                    {
+                        col.cards = col.cards.OrderBy(x => Guid.NewGuid().ToString()).ToList();
+                        break;
+                    }
+                }
+        }
+    }
+}
---
+++ WebAPIapplication/Pack.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPIapplication
+{
+    public class Pack
+    {
+        private static List<string> allCards = new List<string>()
+        { "2черви", "2пики", "2крести", "2буби",
+          "3черви","3пики", "3крести", "3буби",
+          "4черви", "4пики", "4крести", "4буби",
+          "5черви", "5пики", "5крести", "5буби",
+          "6черви","6пики", "6крести", "6буби",
+          "7черви", "7пики", "7крести", "7буби",
+          "8черви", "8пики", "8крести", "8буби",
+          "9черви","9пики", "9крести", "9буби",
+          "10черви", "10пики", "10крести", "10буби",
+          "Вчерви", "Впики", "Вкрести", "Вбуби",
+          "Дчерви","Дпики", "Дкрести", "Дбуби",
+          "Кчерви", "Кпики", "Ккрести", "Кбуби",
+          "Тчерви", "Тпики", "Ткрести", "Тбуби",
+        };
+        
+        public string name;
+        public int countCards;
+        public List<string> cards;
+
+        public Pack(string name, int countCards)
+        {
+            this.name = name;
+            this.countCards = countCards;
+            cards = allCards.GetRange(0, this.countCards);
+        }
+
+    }
+}
